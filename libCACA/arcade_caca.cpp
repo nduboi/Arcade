c68300aa965d22d49d1bea1e739456b7b8b977d6
Nduboi <63715705+nduboi@users.noreply.github.com>
2025-03-20 15:29:12 +0100

    [ADD] Add a test shared library

---
+++ libCACA/arcade_caca.cpp
@@ -0,0 +1,26 @@
+//
+// Created by roussierenoa on 3/17/25.
+//
+
+#include "arcade_caca.hpp"
+
+#include <iostream>
+
+void arcadeCaca::initWindow() {
+}
+
+void arcadeCaca::display() {
+}
+
+void arcadeCaca::closeWindow() {
+}
+
+bool arcadeCaca::isOpen() {
+	return false;
+}
+
+void arcadeCaca::clear() {
+}
+
+arcadeCaca::arcadeCaca() {
+}
