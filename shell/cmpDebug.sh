1aa8f20e38278e8a7bcc4b13471b48d4282d798e
Nduboi <63715705+nduboi@users.noreply.github.com>
2025-03-21 09:53:20 +0100

    [REFACTO] Refacto terget_include_directrory

--- shell/cmpDebug.sh
+++ shell/cmpDebug.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 cmake . -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Debug -DCMAKE_RUNTIME_OUTPUT_DIRECTORY=./ -B my-build-debug
 cmake --build my-build-debug -j
-cp ./my-build-debug/src/arcade ./
