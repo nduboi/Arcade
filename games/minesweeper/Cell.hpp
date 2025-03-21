1e3216135bc8ae8cc335c21d02b6afc1cd21a49f
Nolan Papa <nolan.papa@epitech.eu>
2025-03-21 17:13:12 +0100

    [UPDATE] class template for Cell function now heritate of Aentities and not IEntities

--- games/minesweeper/Cell.hpp
+++ games/minesweeper/Cell.hpp
@@ -8,9 +8,9 @@
 #ifndef CELL_HPP_
 #define CELL_HPP_
 
-#include "IEntity.hpp"
+#include "AEntity.hpp"
 
-class Cell : public IEntity {
+class Cell : public AEntity {
     public:
         // Constructor
         Cell(size_t x, size_t y);
@@ -18,24 +18,18 @@ class Cell : public IEntity {
 
         // Method
         gameState_t onClick(grid_t &grid, clickType_t type) override;
-        gameState_t moveEntity(grid_t &grid) override;
-        gameState_t moveEntity(grid_t &grid, std::pair<size_t, size_t> direction) override;
         gameState_t onInteract(grid_t &grid) override;
-        bool isMovable() const override;
-        bool hasCollisions() const override;
         bool isRevealed() const;
         bool isFlagged() const;
         bool isMine() const;
 
         // Getter
-        std::pair<size_t, size_t> getPosition() const override;
         std::string getSpriteName() const override;
         std::size_t getColor() const override;
         std::string getText() const override;
         size_t getAdjacentMines() const;
 
         // Setter
-        void setPosition(std::pair<size_t, size_t> position) override;
         void setRevealed(bool revealed);
         void setFlagged(bool flagged);
         void setMine(bool mine);
@@ -45,7 +39,6 @@ class Cell : public IEntity {
 
     private:
         // Variable
-        std::pair<size_t, size_t> _position;
         bool _isRevealed;
         bool _isFlagged;
         bool _isMine;
