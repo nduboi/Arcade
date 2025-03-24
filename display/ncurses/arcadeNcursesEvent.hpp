e9b71469b118579f24c61e5860d08b485a4cce80
Nolan Papa <nolan.papa@epitech.eu>
2025-03-24 18:19:44 +0100

    [ADD] event class for ncurses

---
+++ display/ncurses/arcadeNcursesEvent.hpp
@@ -0,0 +1,31 @@
+//
+// Created by roussierenoa on 3/20/25.
+//
+
+#ifndef ARCADE_NCURSES_EVENT_HPP
+#define ARCADE_NCURSES_EVENT_HPP
+
+#include "IEvent.hpp"
+#include "IWindow.hpp"
+#include <ncurses.h>
+
+class arcadeNcursesEvent : public IEvent {
+	public:
+		// Constructor
+		explicit arcadeNcursesEvent(IWindow &window);
+		~arcadeNcursesEvent() override = default;
+
+		// Method
+		void init() override;
+		IEvent::event_t pollEvents(std::pair<int, int> gridSize) override;
+		void cleanup() override;
+
+		// Getter
+		std::pair<int, int> getMousePos() override;
+
+	private:
+		// Variable
+		IWindow &_window;
+};
+
+#endif // ARCADE_NCURSES_EVENT_HPP
