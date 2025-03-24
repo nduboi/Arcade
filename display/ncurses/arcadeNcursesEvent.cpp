62e5483f9a19e72a4a054f3bab1e2ae98b7118a4
Nolan Papa <nolan.papa@epitech.eu>
2025-03-24 18:19:31 +0100

    [ADD] event for ncurses

---
+++ display/ncurses/arcadeNcursesEvent.cpp
@@ -0,0 +1,43 @@
+/*
+** EPITECH PROJECT, 2025
+** arcade
+** File description:
+** arcadeNcursesEvent
+*/
+
+#include "arcadeNcursesEvent.hpp"
+
+arcadeNcursesEvent::arcadeNcursesEvent(IWindow &window) : _window(window)
+{
+
+}
+
+void arcadeNcursesEvent::init()
+{
+
+}
+
+IEvent::event_t arcadeNcursesEvent::pollEvents(std::pair<int, int> gridSize)
+{
+    int ch = getch();
+
+    switch (ch) {
+        case KEY_UP: return IEvent::UP;
+        case KEY_DOWN: return IEvent::DOWN;
+        case KEY_LEFT: return IEvent::LEFT;
+        case KEY_RIGHT: return IEvent::RIGHT;
+        case 'q': return IEvent::CLOSE;
+        default: return IEvent::NOTHING;
+    }
+}
+
+std::pair<int, int> arcadeNcursesEvent::getMousePos()
+{
+    // je sais pas trop comment faire
+    return {0, 0};
+}
+
+void arcadeNcursesEvent::cleanup()
+{
+
+}
