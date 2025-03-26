8161c5172c2a72154195acb38ea1097b2f328e1c
Nolan Papa <nolan.papa@epitech.eu>
2025-03-26 15:45:27 +0100

    [UPDATE] logic in minesweeper class

--- games/minesweeper/MinesweeperGame.cpp
+++ games/minesweeper/MinesweeperGame.cpp
@@ -18,10 +18,10 @@ MinesweeperGame::MinesweeperGame(size_t width, size_t height)
 
 void MinesweeperGame::initializeGrid()
 {
-    _entities.resize(_height);
-    for (size_t y = 0; y < _height; ++y) {
-        _entities[y].resize(_width);
-        for (size_t x = 0; x < _width; ++x) {
+    _entities.resize(this->_height);
+    for (size_t y = 0; y < this->_height; ++y) {
+        _entities[y].resize(this->_width);
+        for (size_t x = 0; x < this->_width; ++x) {
             _entities[y][x].resize(1);
             _entities[y][x][0] = std::make_shared<Cell>(x, y);
         }
