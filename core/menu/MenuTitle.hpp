75b5ba19a444d23f87b990931467d46d14478edb
Nolan Papa <nolan.papa@epitech.eu>
2025-03-27 15:00:27 +0100

    [ADD] class for the title menu

---
+++ core/menu/MenuTitle.hpp
@@ -0,0 +1,42 @@
+/*
+** EPITECH PROJECT, 2025
+** B-OOP-400-NAN-4-1-arcade-eliott.tesnier
+** File description:
+** MenuTitle
+*/
+
+#ifndef MENU_TITLE_HPP
+#define MENU_TITLE_HPP
+
+#include <string>
+#include <memory>
+#include "IWindow.hpp"
+
+class MenuTitle {
+    private:
+        std::shared_ptr<IWindow> _window;
+    public:
+        // Constructor
+        MenuTitle(std::shared_ptr<IWindow> window, const std::string& title = "Arcade Menu",
+            const std::pair<int, int>& position = {100, 50}, int fontSize = 24);
+
+        // Method
+        void draw();
+
+        // Getter
+        std::string getTitle() const;
+
+        // Setter
+        void setTitle(const std::string& title);
+        void setPosition(const std::pair<int, int>& position);
+        void setFontSize(int fontSize);
+
+    private:
+        // Variable
+        std::string _title;
+        std::pair<int, int> _position;
+        int _fontSize;
+
+};
+
+#endif // MENU_TITLE_HPP
