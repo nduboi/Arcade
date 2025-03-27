7217dfb65d885a2f80c64bd40c61195b41e831b3
Nduboi <63715705+nduboi@users.noreply.github.com>
2025-03-27 13:10:55 +0100

    [WIP] Adding SDl file

---
+++ display/SDL/SDLEncapsulation.cpp
@@ -0,0 +1,5 @@
+//
+// Created by roussierenoa on 3/27/25.
+//
+
+#include "SDLEncapsulation.hpp"
---
+++ display/SDL/SDLEncapsulation.hpp
@@ -0,0 +1,16 @@
+//
+// Created by roussierenoa on 3/27/25.
+//
+
+#ifndef SDLENCAPSULATION_HPP
+#define SDLENCAPSULATION_HPP
+
+
+
+class SDLEncapsulation {
+
+};
+
+
+
+#endif //SDLENCAPSULATION_HPP
