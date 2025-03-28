c5c734e48e2d6cfa1a317008139e1ee0560ed09d
eliotttesnier <eliott.tesnier@epitech.eu>
2025-03-28 14:24:03 +0100

    [ADD] HighScore entity

---
+++ games/entities/HighScoreEntityHUD.cpp
@@ -0,0 +1,19 @@
+/*
+** EPITECH PROJECT, 2025
+** arcade
+** File description:
+** HighScoreEntityHUD
+*/
+
+#include "HighScoreEntityHUD.hpp"
+
+HighScoreEntityHUD::HighScoreEntityHUD(std::size_t score, std::pair<std::size_t, std::size_t> position)
+{
+    this->_color = 1;
+    this->_text = "Highscore: " + std::to_string(score);
+    this->_position = position;
+    this->_isMovable = false;
+    this->_hasCollisions = false;
+    this->_isControlable = false;
+    this->_spriteName = "";
+}
