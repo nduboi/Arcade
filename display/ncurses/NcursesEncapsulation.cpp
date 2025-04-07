93a08d9dc6cfe60ea10e7b12e81be02642e8f281
Nduboi <63715705+nduboi@users.noreply.github.com>
2025-04-07 19:40:48 +0200

    [REFACTO] adjust Ncurses rendering logic for improved map positioning and scaling

--- display/ncurses/NcursesEncapsulation.cpp
+++ display/ncurses/NcursesEncapsulation.cpp
@@ -78,13 +78,11 @@ namespace Display {
 			wattron(this->_game, COLOR_PAIR(color));
 		}
 		std::pair<int, int> currentPos = {pos};
-		int startY = (getmaxy(this->_game) - mapSize.second - 4) / 2;
-		int startX = ((getmaxx(this->_game) - mapSize.first - 4) / 2);
-		currentPos.second += startY;
-		currentPos.first += startX;
-		for (int y = 0; y < size.second; y++) {
-			for (int x = 0; x < size.first; x++) {
-				mvwaddch(this->_game, currentPos.second + y, currentPos.first + x, ' ' | A_REVERSE);
+		int startY = (getmaxy(this->_game)) / 2 - mapSize.second;
+		int startX = ((getmaxx(this->_game)) / 2 - mapSize.first * 2);
+		for (int y = 0; y < size.second * 2; y++) {
+			for (int x = 0; x < size.first * 2; x++) {
+				mvwaddch(this->_game, currentPos.second * 2 + startY + y, currentPos.first * 2 + startX  + x, ' ' | A_REVERSE);
 			}
 		}
 		if (color > 0) {
