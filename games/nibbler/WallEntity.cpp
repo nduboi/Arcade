8532efa13f8c8022aa5547172c1935257f5f0e50
Nolan Papa <nolan.papa@epitech.eu>
2025-04-09 16:21:04 +0200

    [ADD] Wall entity class for the nibbler game

---
+++ games/nibbler/WallEntity.cpp
@@ -0,0 +1,19 @@
+/*
+** EPITECH PROJECT, 2025
+** B-OOP-400-NAN-4-1-arcade-eliott.tesnier
+** File description:
+** WallEntity
+*/
+
+#include "WallEntity.hpp"
+
+WallEntity::WallEntity(const std::string& spriteName, std::pair<size_t, size_t> position)
+{
+    this->_spriteName = spriteName;
+    this->_color = 5;
+    this->_text = "#";
+    this->_position = position;
+    this->_isMovable = false;
+    this->_isControlable = false;
+    this->_hasCollisions = true;
+}
