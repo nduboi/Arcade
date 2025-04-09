285914bd31f24cf57bf02dcb0ac00b8406212255
Nolan Papa <nolan.papa@epitech.eu>
2025-04-09 16:19:40 +0200

    [ADD] food entity class for the nibbler game

---
+++ games/nibbler/FoodEntity.hpp
@@ -0,0 +1,28 @@
+/*
+** EPITECH PROJECT, 2025
+** B-OOP-400-NAN-4-1-arcade-eliott.tesnier
+** File description:
+** FoodEntity
+*/
+
+#ifndef FOODENTITY_HPP_
+#define FOODENTITY_HPP_
+
+#include "AEntity.hpp"
+
+class FoodEntity : public AEntity {
+    public:
+        // Constructor
+        FoodEntity(std::pair<size_t, size_t> position);
+        ~FoodEntity();
+
+        // Method
+        void onInteract(std::shared_ptr<IGameModule> gameModule) override;
+
+    private:
+        //Method
+        int getNbVoidEntities(const grid_t &grid) const;
+        bool isValidPosition(const std::pair<size_t, size_t> &pos, const grid_t &grid) const;
+};
+
+#endif /* !FOODENTITY_HPP_ */
