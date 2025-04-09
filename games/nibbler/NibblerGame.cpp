3072da3414ab24ddbd578a8bf38ed2092cc551f7
Nolan Papa <nolan.papa@epitech.eu>
2025-04-09 16:20:20 +0200

    [ADD] Nibbler game class for the nibbler game

---
+++ games/nibbler/NibblerGame.cpp
@@ -0,0 +1,234 @@
+/*
+** EPITECH PROJECT, 2025
+** B-OOP-400-NAN-4-1-arcade-eliott.tesnier
+** File description:
+** NibblerGame
+*/
+
+#include "NibblerGame.hpp"
+#include "VoidEntity.hpp"
+#include "NibblerHeadEntity.hpp"
+#include "NibblerTailEntity.hpp"
+#include "FoodEntity.hpp"
+#include "WallEntity.hpp"
+
+#include "ScoreEntityHUD.hpp"
+#include "HighScoreEntityHUD.hpp"
+#include "TimeEntityHUD.hpp"
+#include "BigTextEntityHUD.hpp"
+#include <bits/algorithmfwd.h>
+
+const int MAP_HEIGHT = 21;
+const int MAP_WIDTH = 21;
+const int MAP_LAYER = 2;
+
+NibblerGame::NibblerGame() : _currentLevel(1)
+{
+    this->_highScore = 0;
+    this->_score = 0;
+    this->_isStarted = false;
+    this->_gameState = PLAYING;
+    this->_time = std::chrono::steady_clock::now();
+
+    _mapManager.generateMap(_currentLevel);
+
+    this->_entities.resize(MAP_HEIGHT);
+    for (int y = 0; y < MAP_HEIGHT; y++) {
+        this->_entities[y].resize(MAP_WIDTH);
+        for (int x = 0; x < MAP_WIDTH; x++) {
+            this->_entities[y][x].resize(MAP_LAYER);
+        }
+    }
+    this->setLayerBackground();
+    this->setLayerWalls();
+    this->setLayerEntities();
+    debugPrintMap();
+}
+
+void NibblerGame::setLayerBackground()
+{
+    for (int y = 0; y < MAP_HEIGHT; y++) {
+        for (int x = 0; x < MAP_WIDTH; x++) {
+            this->_entities[y][x][0] = std::make_shared<VoidEntity>("assets/nibbler/Floor.png", 1, ".", std::make_pair(x, y));
+        }
+    }
+}
+
+void NibblerGame::setLayerWalls()
+{
+    auto mapLayout = _mapManager.getMapLayout();
+
+    for (int y = 0; y < MAP_HEIGHT && y < mapLayout.size(); y++) {
+        for (int x = 0; x < MAP_WIDTH && x < mapLayout[y].size(); x++) {
+            if (mapLayout[y][x] >= 1 && mapLayout[y][x] <= 14) {
+                std::string wallSprite = _mapManager.getSpriteFromId(mapLayout[y][x]);
+                std::cout << "mur :" << mapLayout[y][x] << " sprite : " << wallSprite << std::endl;
+                this->_entities[y][x][1] = std::make_shared<WallEntity>(wallSprite, std::make_pair(x, y));
+            }
+        }
+    }
+}
+
+void NibblerGame::setLayerEntities()
+{
+    for (int y = 0; y < MAP_HEIGHT; y++) {
+        for (int x = 0; x < MAP_WIDTH; x++) {
+            if (!this->_entities[y][x][1] || !std::dynamic_pointer_cast<WallEntity>(this->_entities[y][x][1]))
+                this->_entities[y][x][1] = std::make_shared<VoidEntity>(this->_entities[y][x][0]->getSpriteName(), 1, "", std::make_pair(x, y));
+        }
+    }
+
+    auto startPos = _mapManager.getPlayerStartPosition();
+    this->_entities[startPos.second][startPos.first][1] = std::make_shared<NibblerHeadEntity>(4, "h", startPos);
+    this->setNibblerBody();
+    this->setLayerFood();
+}
+
+void NibblerGame::setNibblerBody()
+{
+    auto startPos = _mapManager.getPlayerStartPosition();
+    auto validDirections = _mapManager.getValidDirections(startPos);
+
+    if (validDirections.empty())
+        return;
+
+    std::pair<int, int> bodyDirection = {-validDirections[0].first, -validDirections[0].second};
+
+    std::vector<std::pair<size_t, size_t>> bodyPositions;
+    std::pair<size_t, size_t> currentPos = startPos;
+
+    for (int i = 0; i < 3; i++) {
+        std::pair<size_t, size_t> nextPos = {
+            currentPos.first + bodyDirection.first,
+            currentPos.second + bodyDirection.second
+        };
+
+        if (nextPos.first >= MAP_WIDTH || nextPos.second >= MAP_HEIGHT ||
+            nextPos.first < 0 || nextPos.second < 0 ||
+            _mapManager.isWallAt(nextPos))
+            break;
+
+        bodyPositions.push_back(nextPos);
+        currentPos = nextPos;
+    }
+
+    for (size_t i = 0; i < bodyPositions.size(); i++) {
+        auto pos = bodyPositions[i];
+        this->_entities[pos.second][pos.first][1] = std::make_shared<NibblerTailEntity>(3, "b", pos, i);
+    }
+
+    if (bodyPositions.size() >= 1) {
+        auto body1 = std::dynamic_pointer_cast<NibblerTailEntity>(this->_entities[bodyPositions[0].second][bodyPositions[0].first][1]);
+        if (body1) {
+            std::pair<size_t, size_t> nextPos = bodyPositions.size() >= 2 ? bodyPositions[1] : std::pair<size_t, size_t>{100, 100};
+            body1->updateDirection(bodyPositions[0], startPos, nextPos);
+        }
+    }
+
+    if (bodyPositions.size() >= 2) {
+        auto body2 = std::dynamic_pointer_cast<NibblerTailEntity>(this->_entities[bodyPositions[1].second][bodyPositions[1].first][1]);
+        if (body2) {
+            std::pair<size_t, size_t> nextPos = bodyPositions.size() >= 3 ? bodyPositions[2] : std::pair<size_t, size_t>{100, 100};
+            body2->updateDirection(bodyPositions[1], bodyPositions[0], nextPos);
+        }
+    }
+
+    if (bodyPositions.size() >= 3) {
+        auto body3 = std::dynamic_pointer_cast<NibblerTailEntity>(this->_entities[bodyPositions[2].second][bodyPositions[2].first][1]);
+        if (body3) {
+            body3->updateDirection(bodyPositions[2], bodyPositions[1], std::make_pair(100, 100));
+        }
+    }
+
+    auto head = std::dynamic_pointer_cast<NibblerHeadEntity>(this->_entities[startPos.second][startPos.first][1]);
+    if (head) {
+        std::vector<std::pair<size_t, size_t>> allPositions = bodyPositions;
+        allPositions.insert(allPositions.begin(), startPos);
+        std::reverse(allPositions.begin(), allPositions.end());
+        head->initializePreviousPositions(allPositions);
+    }
+}
+
+std::pair<size_t, size_t> NibblerGame::findFoodPosition() const
+{
+    std::vector<std::pair<size_t, size_t>> emptyPositions;
+
+    for (int y = 0; y < MAP_HEIGHT; y++) {
+        for (int x = 0; x < MAP_WIDTH; x++) {
+            if (std::dynamic_pointer_cast<VoidEntity>(this->_entities[y][x][1])) {
+                emptyPositions.push_back(std::make_pair(x, y));
+            }
+        }
+    }
+
+    if (emptyPositions.empty())
+        return std::make_pair(0, 0);
+
+    int randIndex = rand() % emptyPositions.size();
+    return emptyPositions[randIndex];
+}
+
+void NibblerGame::setLayerFood()
+{
+    auto foodPos = findFoodPosition();
+    this->_entities[foodPos.second][foodPos.first][1] = std::make_shared<FoodEntity>(foodPos);
+}
+
+bool NibblerGame::isWallAt(const std::pair<size_t, size_t>& position) const
+{
+    return _mapManager.isWallAt(position);
+}
+
+std::vector<std::pair<int, int>> NibblerGame::getValidDirections(const std::pair<size_t, size_t>& position) const
+{
+    return _mapManager.getValidDirections(position);
+}
+
+std::vector<std::shared_ptr<IEntity>> NibblerGame::getHUD() const
+{
+    std::vector<std::shared_ptr<IEntity>> hud;
+
+    hud.push_back(std::make_shared<ScoreEntityHUD>(this->getScore(), std::make_pair(10, 15)));
+    hud.push_back(std::make_shared<HighScoreEntityHUD>(this->getHighScore(), std::make_pair(10, 55)));
+
+    std::size_t secondsElapsed = this->getTime();
+    hud.push_back(std::make_shared<TimeEntityHUD>(secondsElapsed, std::make_pair(350, 35)));
+
+    if (this->getIsStarted() == false)
+        hud.push_back(std::make_shared<BigTextEntityHUD>("Press any direction to start", std::make_pair(250, 850)));
+    if (this->getGameState() == LOSE)
+        hud.push_back(std::make_shared<BigTextEntityHUD>("Game Over", std::make_pair(350, 850)));
+    if (this->getGameState() == WIN)
+        hud.push_back(std::make_shared<BigTextEntityHUD>("You Win", std::make_pair(350, 850)));
+
+    return hud;
+}
+
+void NibblerGame::debugPrintMap() const
+{
+    auto mapLayout = _mapManager.getMapLayout();
+    std::cout << "Map layout:" << std::endl;
+    for (const auto& row : mapLayout) {
+        for (int cell : row) {
+            std::cout << " " << cell;
+        }
+        std::cout << std::endl;
+    }
+
+    for (int y = 0; y < MAP_HEIGHT; y++) {
+        for (int x = 0; x < MAP_WIDTH; x++) {
+            auto entity = _entities[y][x][1];
+            if (std::dynamic_pointer_cast<NibblerHeadEntity>(entity))
+                std::cout << "H";
+            else if (std::dynamic_pointer_cast<NibblerTailEntity>(entity))
+                std::cout << "T";
+            else if (std::dynamic_pointer_cast<WallEntity>(entity))
+                std::cout << "#";
+            else if (std::dynamic_pointer_cast<FoodEntity>(entity))
+                std::cout << "F";
+            else
+                std::cout << ".";
+        }
+        std::cout << "\n";
+    }
+}
